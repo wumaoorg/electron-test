b555cfb3aaacd954e7c6f635efbb87b06dd02610
wumao <webwwy@gmail.com>
2020-05-25 21:55:05 +0800

    测试用的

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,100 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="title-bar">
+      <div class="title">
+        <div class="logo">
+          <img src="@/assets/logo.png" alt />
+        </div>
+        <div class="txt">
+          <button @click="close">关闭</button>
+          <ul>
+            <li>个性推荐</li>
+            <li>歌单</li>
+            <li>主播电台</li>
+            <li>排行榜</li>
+            <li>歌手</li>
+            <li>最新音乐</li>
+            <li>
+              <input type="text" placeholder="搜索" />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="window-tool">
+        <div></div>
+      </div>
     </div>
-    <router-view/>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
+<script>
+let { remote } = require("electron");
+export default {
+  methods: {
+    close() {
+      remote.getCurrentWindow().maximize();
+    },
+    // 防抖
+    debounce(fn) {
+      let timeout = null;
+      const a = 2
+      return function() {
+        console.log(a);
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          fn.apply(this, arguments);
+        }, 1000);
+      };
+    }
+  },
+  mounted() {
+    let win = remote.getCurrentWindow();
+    win.on(
+      "resize",
+      this.debounce(() => {
+        console.log("123");
+      })
+    );
+  }
+};
+</script>
+
 <style lang="scss">
+@import url("./assets/reset.css");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  .title-bar {
+    background-color: #f9f9f9;
+    .title {
+      -webkit-app-region: drag;
+      display: flex;
+      justify-content: space-between;
+      .logo {
+        flex: 1;
+        margin-right: 90px;
+        img {
+          width: 50px;
+          height: 50px;
+        }
+      }
+      .txt {
+        flex: 9;
+        ul {
+          display: flex;
+          flex-wrap: nowrap;
+          justify-content: space-around;
+          // display: inline-block;
+          li {
+            float: left;
+            color: #646464;
+            font-size: 14px;
+            line-height: 50px;
+          }
+        }
+      }
     }
   }
 }
